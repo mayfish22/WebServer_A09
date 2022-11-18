0919f6b3d192f0cb81b8d726cce378bb09a190b9
mayfish <mayfish22@gmail.com>
2022-11-19 01:23:43 +0800

    加入專案檔案。

---
+++ WebServer/Controllers/WebApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebServer.Models.WebServerDB;
+
+namespace WebServer.Controllers
+{
+    [Route("api")]
+    //使用JwtBearer
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class WebApiController : Controller
+    {
+        private readonly WebServerDBContext _WebServerDBContext;
+        private readonly IHttpContextAccessor _context;
+        public WebApiController(WebServerDBContext WebServerDBContext,
+            IHttpContextAccessor context)
+        {
+            _WebServerDBContext = WebServerDBContext;
+            _context = context;
+        }
+        // api/Test
+        [HttpPost("Test")]
+        public async Task<IActionResult> Test()
+        {
+            try
+            {
+                var userID = User?.Identity?.Name;
+                var user = await _WebServerDBContext.User.FindAsync(userID);
+                return Json(new
+                {
+                    email = user?.Email,
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
